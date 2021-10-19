43a6c5ba27841790578ddc79ed0574787bcdf2d9
itsmevc7 <92775257+itsmevc7@users.noreply.github.com>
2021-10-19 11:28:57 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "aws_access_key" {
-default = "AKIAZXSDYZHGS4JA7PNZ"
+ default = "AKIAZXSDYZHGS4JA7PNZ"
 }
 variable "aws_secret_key" {
-default = "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"
+ default = "YyWysoZFZChWoShk5W4gMfo1oP/kcN7YnpRBO1s+"
 }
