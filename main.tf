42ce7069e9681295759304dcc78a7baa8e1de614
itsmevc7 <92775257+itsmevc7@users.noreply.github.com>
2021-10-19 20:20:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "barshicorp/aws"
-      version = "~> 3.0"
-    }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Vishnu C"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = False
+  enable_vpn_gateway = False
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
-}
-# Configure the AWS Providers
-providers "aws" {
-  region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
