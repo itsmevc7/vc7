82a36e5828d41fd71ac7eb3181a6ad7ba27be7d4
itsmevc7 <92775257+itsmevc7@users.noreply.github.com>
2021-10-19 20:17:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
-# Create a VPC
-resource "aws_vpc""cloudbolt"{
-  cidr_block = "10.0.0.0/16"
+terraform {
+  required_providers {
+    aws = {
+      source = "barshicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+# Configure the AWS Providers
+providers "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
